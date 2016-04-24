062e277397dedb8a4dccf9395feb3a3564b73d32
Win10Lxp <xplixina@sina.com>
2016-04-24 17:31:16 +0800

    初步实现了获取类加载的目录

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,5 +1,9 @@
 /**
  * Created by Win10Lxp on 2016/4/16.
+ * CopyAppA  程序名称 系统数据库名称 用户数据库名称
+ * 当前目录就是目标程序所在目录
+ * 系统数据库：程序名称_systemdb
+ * 用户数据库：程序名称_userdb
  */
 class Main {
     /**
@@ -14,14 +18,28 @@ class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //变量定义
         def seedApplicationPath
         def seedProperties = new java.util.Properties()
 
-        args.each {e->
-            println(e)
+        //处理命令行
+        if (args.length < 1) {
+            println("程序使用方法：")
+            println("CopyAppA  程序名称 系统数据库名称 用户数据库名称")
+            println("当前目录就是目标程序所在目录")
+            println("系统数据库：程序名称_systemdb")
+            println("用户数据库：程序名称_userdb")
+        } else {
+            println("当前命令行：")
+            args.each { e ->
+                println(e)
+            }
         }
+
+        Main main = new Main()
+        def currentPath = main.getCurrentPath()
         //读取配置文件，获取种子程序的位置
-        def iniFile = new File("SeedApplication.ini")
+        def iniFile = new File("${currentPath}/SeedApplication.ini")
         if (iniFile.exists()) {
             def fis = new FileInputStream(iniFile)
             InputStreamReader isr = new InputStreamReader(fis, "UTF-8")
@@ -34,17 +52,57 @@ class Main {
             println("SeedApplication.ini 文件不存在！")
         }
         //分析参数，生成目标程序的路径
-        xcopyApplication(args, seedProperties)
         //调用系统命令，拷贝文件
+        xcopyApplication(args, seedProperties)
         //修改文件
+        //修改程序名----
+
+        //创建类对象
+        def targetApplication = new TargetApplication()
+        targetApplication.processArgs(args)
+        println(targetApplication)
+        //println(this.getClass().getResource("/").getPath())
+
+        //System.out.println(System.getProperty("java.class.path"));    //成功，但是不是想要的结果
+
     }
 
     /**
      * 获取当前目录
      * */
-    def static currentPath() {
+    def static getCurrentRunPath() {
         def f = new File(".")
         return f.canonicalPath
+    }
+
+    /*
+    *获取当前目录
+    * */
+    def getCurrentPath() {
+        println("当前类加载目录是：")
+        def loader = this.getClass().getClassLoader()
+        //def res = loader.getResources("")
+        //res.each {it->
+        //    println("hi ${it}")
+        //}
+        //println("-------")
+        //println(res)
+        //println(loader.getResource("Main.class").getPath())
+        //println(loader.getResource("Main.class").getFile())
+        def temp = loader.getResource("Main.class").getFile()
+        def k = temp.indexOf("Main.class")
+        def tempa = temp.substring(0, k-1)
+        //println("-------${tempa}")
+        if (tempa.indexOf(".jar!")) {
+            k = tempa.lastIndexOf("/")
+            tempa = tempa.substring(0, k)
+        }
+        println("-------${tempa}")
+        if (tempa.indexOf("file:/")>-1) {
+            tempa = tempa.substring(5)
+        }
+        println("-------${tempa}")
+        return tempa
     }
 
     /**
@@ -55,16 +113,16 @@ class Main {
         def targetApplication
         def source = properties.getProperty("主目录")
         def targetPath
-        if (args.length>0) {
+        if (args.length > 0) {
             targetApplication = args[0]
             if (!targetApplication.contains("\\")) {
-                targetApplication = currentPath() + "\\" + targetApplication
+                targetApplication = getCurrentRunPath() + "\\" + targetApplication
             }
             println("目标：" + targetApplication)
         }
         def commond = "xcopy ${source} ${targetApplication} /E /I /Y"
         println(commond)
-        Runtime runtime=Runtime.getRuntime()
+        Runtime runtime = Runtime.getRuntime()
         try {
             runtime.exec(commond)
         } catch (Exception e) {
--- src/TargetApplication.groovy
+++ src/TargetApplication.groovy
@@ -2,4 +2,54 @@
  * Created by Win10Lxp on 2016/4/24.
  */
 class TargetApplication {
+    String appName
+    String appPath
+    String systemDB
+    String userDB
+
+    public TargetApplication() {
+        appPath = Main.getCurrentRunPath();
+        appName ="NoNamed"
+        systemDB = "NoNamed"
+        userDB = "NoNamed"
+    }
+
+    String toString() {
+        return "${appName}/(${systemDB},${userDB})@${appPath}"
+    }
+
+    def processArgs(String[] args) {
+        def argsCount = args.length
+
+        switch (argsCount) {
+            case 0:
+                appName ="NoNamed"
+                systemDB = "NoNamed"
+                userDB = "NoNamed"
+                break
+            case 1:
+                //只有一个参数
+                appName = args[0]
+                systemDB = appName + "_systemDB"
+                userDB = appName + "_userDB"
+                break
+            case 2:
+                //有两个参数
+                appName = args[0]
+                systemDB = args[1]
+                userDB = appName + "_userDB"
+                break
+            case 3:
+                //有三个参数
+                appName = args[0]
+                systemDB = args[1]
+                userDB = args[2]
+                break
+            default:
+                appName = args[0]
+                systemDB = args[1]
+                userDB = args[2]
+                break
+        }
+    }
 }
